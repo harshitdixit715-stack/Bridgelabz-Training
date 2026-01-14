f79a8840570c97f2a18e666a0617a63cd1c7bf35
Harshit Dixit <harshitdixit715@gmail.com>
2026-01-15 00:42:06 +0530

    Level1-2

---
+++ basic-program/SamMarks.java
@@ -0,0 +1,9 @@
+public class SamMarks {
+    public static void main(String[] args) {
+        int Maths = 94;
+        int Physics = 95;
+        int Chemistry = 96;
+        double Average = (Maths+Physics+Chemistry)/3.0;
+        System.out.println("Sam's average marks in PCM is " + Average);
+    }
+}
