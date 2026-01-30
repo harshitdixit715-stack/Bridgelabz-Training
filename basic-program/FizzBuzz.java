92cbd89a44ff46ece8e71f64e8eaba63f2ad41ca
Harshit Dixit <harshitdixit715@gmail.com>
2026-01-30 22:12:49 +0530

    [Harshit] Add.FizzBuzz

---
+++ basic-program/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int n = in.nextInt();
+        int i = 1;
+            while(i<=n){
+                if(i%3 == 0 && i%5==0){
+                    System.out.println("FizzBuzz");
+                }
+                else if(i%3 == 0){
+                    System.out.println("Fizz");
+                }
+                else if(i%5 == 0){
+                    System.out.println("Buzz");
+                }
+                else{
+                    System.out.println(i);
+                }
+                i++;
+            }
+
+        in.close();
+    }
+}
